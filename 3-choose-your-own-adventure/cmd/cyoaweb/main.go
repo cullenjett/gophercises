eb68b4a9e483cdabc455626f0f64abac898a6806
Cullen Jett <cullenjett@gmail.com>
2019-03-09 10:11:55 -0700

    add an http handler and template to render the story

--- 3-choose-your-own-adventure/cmd/cyoaweb/main.go
+++ 3-choose-your-own-adventure/cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"cyoa"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "Port where the web server is run")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- 3-choose-your-own-adventure/story.go
+++ 3-choose-your-own-adventure/story.go
@@ -3,7 +3,54 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Choose your own adventure</title>
+</head>
+<body>
+  <h1>{{.Title}}</h1>
+
+  {{range .Paragraphs}}
+  <p>{{.}}</p>
+  {{end}}
+
+  <ul>
+    {{range .Options}}
+    <li>
+      <a href="/{{.Chapter}}">{{.Text}}</a>
+    </li>
+    {{end}}
+  </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{
+		s,
+	}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.New("").Parse(defaultHandlerTmpl))
+
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
